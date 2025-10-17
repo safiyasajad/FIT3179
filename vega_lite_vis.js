544378efaec4f2194385c4f3c20aa890256d8d69
safiyasajad <131326330+safiyasajad@users.noreply.github.com>
2025-10-17 11:44:36 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -6,4 +6,14 @@ vegaEmbed('#map_idiom', vg_1).then(function(result) {
 // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var vg_2 = 'stacked_bar_chart_vg.json'
+
+
+// Embed stacked bar chart
+vegaEmbed('#stacked_bar_chart', vg_2).then(function(result) {
+// Access the Vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
 
